219357a4853cd5aea37244bd25e76b434c729e11
leviholt753 <leviholt@mail.weber.edu>
2024-04-08 17:03:15 -0600

    question 6-18

--- cars.py
+++ cars.py
@@ -3,3 +3,19 @@ cars = ['Ford','Chrysler','Dodge','Ram','Jeep','Chevy','GMC']
 print ("cars")
 print(len(cars))
 cars.append("Buick")
+print ("cars")
+print (cars[4])
+cars.insert(3, "Toyota")
+print ("cars")
+cars.pop(5)
+print ("cars")
+cars.sort()
+print("cars")
+cars.sort(reverse=True)
+print ("cars")
+my_array_length =len(cars)
+array_string =len(my_array_length)
+x = [array_string]
+y = [my_array_length]
+z = [x+y]
+print ("z")
